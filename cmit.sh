6ee70afb5db9dfabdc805845817f342e3ca7c22e
Qian Feng <qian.feng1@samsung.com>
2017-07-21 17:58:24 -0700

    “update”

--- cmit.sh
+++ cmit.sh
@@ -1,6 +1,6 @@
 hexo generate
-cp -R public/* .deploy/jiji262.github.io
-cd .deploy/jiji262.github.io
+cp -R public/* .deploy/qian-feng.github.io
+cd .deploy/qian-feng.github.io
 git add .
 git commit -m “update”
 git push origin master
